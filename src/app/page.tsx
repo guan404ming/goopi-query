a4e57dc852661aa81a3a3ada2d5b3078f8c095d3
GUAN MING <guanmingchiu@gmail.com>
2024-02-04 16:43:39 +0800

    feat: add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Banner } from "@/components/banner";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen w-full flex-col items-center justify-between pt-24">
+      <Banner />
+    </main>
   );
 }
---
+++ src/components/banner.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import * as React from "react";
+
+import Autoplay from "embla-carousel-autoplay";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+
+export function Banner() {
+  return (
+    <Carousel
+      className="w-full"
+      plugins={[
+        Autoplay({
+          delay: 4000,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div>
+              <Card>
+                <CardContent className="flex h-[250px] items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
